def8b7fb44a29ee1ec89030e6dccb71acd1ad68b
vgrigoriu <vgrigoriu@cd77b782-3126-0410-85d0-d72a11eec3b5>
2009-06-02 23:15:11 +0000

    Added list of all accepted words for Romanian.
    git-svn-id: http://rebus.googlecode.com/svn@7 cd77b782-3126-0410-85d0-d72a11eec3b5

--- trunk/Rebus/RebusTest/WordListTest.cs
+++ trunk/Rebus/RebusTest/WordListTest.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.IO;
+
+using NUnit.Framework;
 
 using RebusLib;
 
@@ -21,6 +23,16 @@ namespace RebusTest
             Assert.That(list, Is.All.InstanceOf(typeof(Word)));
             Assert.That(List.Map(list).Property("Value"), Is.EqualTo(strings));
             Assert.That(List.Map(list).Property("Weight"), Is.EqualTo(weights));
+        }
+
+        [Test]
+        public void AddWordsFromFile()
+        {
+            string[] words = File.ReadAllLines(@"..\..\..\data\words.ro-ro.txt");
+            WordList list = new WordList();
+            list.Add(words, 7);
+            Assert.That(list.Count, Is.EqualTo(134259));
+            Assert.That(List.Map(list).Property("Weight"), Has.All.EqualTo(7));
         }
     }
 }
