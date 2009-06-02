21bb8bb8b40eff8f2182f7d062f62b5e9e9e6bf1
vgrigoriu <vgrigoriu@cd77b782-3126-0410-85d0-d72a11eec3b5>
2009-06-02 02:13:36 +0000

    git-svn-id: http://rebus.googlecode.com/svn@3 cd77b782-3126-0410-85d0-d72a11eec3b5

---
+++ trunk/Rebus/RebusLib/Word.cs
@@ -0,0 +1,14 @@
+﻿namespace RebusLib
+{
+    public class Word
+    {
+        public string Value { get; set; }
+        public int Weight { get; set; }
+
+        public Word(string word, int weight)
+        {
+            this.Value = word;
+            this.Weight = weight;
+        }
+    }
+}
---
+++ trunk/Rebus/RebusLib/WordList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RebusLib
+{
+    class WordList: List<Word>
+    {
+        public void Add(IEnumerable<string> words, int weight)
+        {
+            foreach (string word in words)
+            {
+                this.Add(new Word(word, weight));
+            }
+        }
+    }
+}
