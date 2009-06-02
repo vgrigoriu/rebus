36e0525b8235fcd717e3cf0ae7a1c8ed1c0c3eba
vgrigoriu <vgrigoriu@cd77b782-3126-0410-85d0-d72a11eec3b5>
2009-06-02 03:59:48 +0000

    More unit tests
    git-svn-id: http://rebus.googlecode.com/svn@5 cd77b782-3126-0410-85d0-d72a11eec3b5

--- trunk/Rebus/RebusTest/WordListTest.cs
+++ trunk/Rebus/RebusTest/WordListTest.cs
@@ -13,8 +13,14 @@ namespace RebusTest
             WordList list = new WordList();
             list.Add(new string[] {"ana", "are", "mere"}, 3);
 
+            string[] strings = new string[] { "ana", "are", "mere" };
+            int[] weights = new int[] { 3, 3, 3 };
+
 
             Assert.That(list.Count, Is.EqualTo(3));
+            Assert.That(list, Is.All.InstanceOf(typeof(Word)));
+            Assert.That(List.Map(list).Property("Value"), Is.EqualTo(strings));
+            Assert.That(List.Map(list).Property("Weight"), Is.EqualTo(weights));
         }
     }
 }
