ed074b31dfdef58ef63b3abcf9ba5a5c8f6e6943
vgrigoriu <vgrigoriu@cd77b782-3126-0410-85d0-d72a11eec3b5>
2009-06-04 05:17:25 +0000

    git-svn-id: http://rebus.googlecode.com/svn@14 cd77b782-3126-0410-85d0-d72a11eec3b5

--- trunk/Rebus/RebusLib/Wordlist.cs
+++ trunk/Rebus/RebusLib/Wordlist.cs
@@ -11,7 +11,7 @@ namespace RebusLib
     {
         private List<Word> wordCollection = new List<Word>();
 
-        public ICollection Words
+        public ICollection<Word> Words
         {
             get
             {
@@ -19,7 +19,7 @@ namespace RebusLib
             }
         }
 
-        public ICollection WordsMatching(Regex regex)
+        public ICollection<Word> WordsMatching(Regex regex)
         {
             return (from Word word in wordCollection
                    where regex.IsMatch(word.Value)
@@ -30,8 +30,35 @@ namespace RebusLib
         {
             foreach (string word in words)
             {
-                this.wordCollection.Add(new Word(word, weight));
+                this.Add(word, weight);
             }
+        }
+
+        public void Add(string value, int weight)
+        {
+            Word word = this.GetWord(value);
+            if (word == null)
+            {
+                word = new Word(value, weight);
+                this.wordCollection.Add(word);
+            }
+            else
+            {
+                Console.WriteLine("Already added word {0}", value);
+                word.Weight = System.Math.Max(weight, word.Weight);
+            }
+        }
+
+        public Word GetWord(string value)
+        {
+            Word word = null;
+            try
+            {
+                word = wordCollection.First(w => w.Value == value);
+            }
+            catch (InvalidOperationException) { /*ignore*/ }
+
+            return word;
         }
     }
 }
--- trunk/Rebus/RebusTest/WordlistTest.cs
+++ trunk/Rebus/RebusTest/WordlistTest.cs
@@ -6,28 +6,38 @@ using NUnit.Framework;
 using RebusLib;
 using System.Collections;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace RebusTest
 {
     [TestFixture]
     public class WordlistTest : AssertionHelper
     {
+        private string[] loPriStrings = new string[] { "ana", "are", "mere" };
+        private string[] hiPriStrings = new string[] { "hopai", "tupai", "diridiridam", "are" };
+        private int hiWeight = 14;
+        private int loWeight = 1;
+
+        private Wordlist wordlist;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            wordlist = new Wordlist();
+            wordlist.Add(loPriStrings, loWeight);
+        }
+
         [Test]
         public void AddWords()
         {
-            string[] strings = new string[] { "ana", "are", "mere" };
-            int weight = 3;
-
-            Wordlist list = new Wordlist();
-            list.Add(strings, weight);
-
-            Expect(list.Words.Count, Is.EqualTo(3));
-            Expect(list.Words, Is.All.InstanceOf(typeof(Word)));
-            foreach (string s in strings)
+            Word[] words = new Word[wordlist.Words.Count];
+            wordlist.Words.CopyTo(words, 0);
+            Expect(words.Length, Is.EqualTo(3));
+            foreach (string s in loPriStrings)
             {
-                Expect(List.Map(list.Words).Property("Value"), Has.Some.EqualTo(s));
+                Expect(List.Map(words).Property("Value"), Has.Some.EqualTo(s));
             }
-            Expect(List.Map(list.Words).Property("Weight"), Has.All.EqualTo(weight));
+            Expect(List.Map(words).Property("Weight"), Has.All.EqualTo(loWeight));
         }
 
         [Test]
@@ -38,24 +48,48 @@ namespace RebusTest
             Wordlist list = new Wordlist();
             list.Add(words, weight);
 
-            Expect(list.Words.Count, Is.EqualTo(134259));
-            Expect(List.Map(list.Words).Property("Weight"), Has.All.EqualTo(weight));
+            Expect(list.Words.Count, Is.EqualTo(134255));
         }
 
         [Test]
         public void WordsMatchingTest()
         {
-            string[] strings = new string[] { "ana", "are", "mere" };
-            int weight = 3;
-
-            Wordlist list = new Wordlist();
-            list.Add(strings, weight);
-
             Regex secondLetterR = new Regex("^.r");
-            ICollection wordsMatching = list.WordsMatching(secondLetterR);
+            ICollection<Word> wordsMatching = wordlist.WordsMatching(secondLetterR);
+            Word[] words = new Word[wordsMatching.Count];
+            wordsMatching.CopyTo(words, 0);
 
-            Expect(wordsMatching.Count, EqualTo(1));
-            Expect(List.Map(wordsMatching).Property("Value"), Has.All.EqualTo("are"));
+            Expect(words.Length, EqualTo(1));
+            Expect(List.Map(words).Property("Value"), Has.All.EqualTo("are"));
+        }
+
+        [Test]
+        public void LowThenHighTest()
+        {
+            Wordlist loHiList = new Wordlist();
+            loHiList.Add(loPriStrings, loWeight);
+            loHiList.Add(hiPriStrings, hiWeight);
+
+            Word are = loHiList.GetWord("are");
+            Expect(are.Weight, Is.EqualTo(hiWeight));
+        }
+
+        [Test]
+        public void HighThenLowTest()
+        {
+            Wordlist hiLoList = new Wordlist();
+            hiLoList.Add(hiPriStrings, hiWeight);
+            hiLoList.Add(loPriStrings, loWeight);
+
+            Word are = hiLoList.GetWord("are");
+            Expect(are.Weight, Is.EqualTo(hiWeight));
+        }
+
+        [Test]
+        public void InexistentWordTest()
+        {
+            Word doesNotExist = this.wordlist.GetWord("dezoxiribonucleic");
+            Expect(doesNotExist, Is.Null);
         }
     }
 }
