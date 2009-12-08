8fa729c565f0ae04fdf3800452affa5a8e33f72f
vgrigoriu <vgrigoriu@cd77b782-3126-0410-85d0-d72a11eec3b5>
2009-12-08 21:54:59 +0000

    versiune nouă, fără mofturi
    git-svn-id: http://rebus.googlecode.com/svn@18 cd77b782-3126-0410-85d0-d72a11eec3b5

---
+++ new/CuvinteÎncrucișate/CuvinteÎncrucișate/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CuvinteÎncrucișate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindowLoaded;
+            this.FileNewMenu.Click += new RoutedEventHandler(FileNewMenuClick);
+        }
+
+        void FileNewMenuClick(object sender, RoutedEventArgs e)
+        {
+            MărimeCareuWindow window = new MărimeCareuWindow();
+            bool? returnValue = window.ShowDialog();
+            if (returnValue.HasValue && returnValue.Value)
+            {
+                int? oriz = window.Orizontal;
+                int? vert = window.Vertical;
+                if (!oriz.HasValue)
+                {
+                    MessageBox.Show("Mărimea pe orizontală nu e-n regulă");
+                    return;
+                }
+                if (!vert.HasValue)
+                {
+                    MessageBox.Show("Mărimea pe verticală nu e-n regulă");
+                    return;
+                }
+                //MessageBox.Show("yeah: " + oriz.Value.ToString() + "x" + vert.Value.ToString());
+                CreateCareu(oriz.Value, vert.Value);
+            }
+            else
+            {
+                MessageBox.Show("nay");
+            }
+        }
+
+        private void CreateCareu(int orizontal, int vertical)
+        {
+            Pătrățel[,] patrate = new Pătrățel[orizontal, vertical];
+            Grid grid = new Grid();
+            this.Panel.Children.Clear();
+            this.Panel.Children.Add(grid);
+            for (int x = 0; x < orizontal; x++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int y = 0; y < vertical; y++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            // create them
+            for (int x = 0; x < orizontal; x++)
+            {
+                for (int y = 0; y < vertical; y++)
+                {
+                    patrate[x, y] = new Pătrățel();
+                }
+            }
+
+            // set their properties
+            for (int x = 0; x < orizontal; x++)
+            {
+                for (int y = 0; y < vertical; y++)
+                {
+                    Pătrățel p = patrate[x, y];
+                    if (x > 0)
+                    {
+                        p.Left = patrate[x - 1, y];
+                    }
+                    if (x < orizontal - 1)
+                    {
+                        p.Right = patrate[x + 1, y];
+                    }
+                    if (y > 0)
+                    {
+                        p.Up = patrate[x, y - 1];
+                    }
+                    if (y < vertical - 1)
+                    {
+                        p.Down = patrate[x, y + 1];
+                    }
+
+                    Grid.SetColumn(p, x);
+                    Grid.SetRow(p, y);
+                    grid.Children.Add(p);
+                }
+            }
+            patrate[0, 0].Focus();
+        }
+
+        void MainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Pătrățel p1 = new Pătrățel();
+            p1.Literă = 'Q';
+            this.Panel.Children.Add(p1);
+            Pătrățel p2 = new Pătrățel();
+            this.Panel.Children.Add(p2);
+            p1.Down = p2;
+            p2.Up = p1;
+            p2.Focus();
+        }
+    }
+}
---
+++ new/CuvinteÎncrucișate/CuvinteÎncrucișate/Pătrățel.xaml.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace CuvinteÎncrucișate
+{
+    /// <summary>
+    /// Interaction logic for Pătrățel.xaml
+    /// </summary>
+    public partial class Pătrățel : UserControl
+    {
+        public Pătrățel()
+        {
+            InitializeComponent();
+            this.GotFocus += new RoutedEventHandler(PătrățelGotFocus);
+            this.LostFocus += new RoutedEventHandler(PătrățelLostFocus);
+            this.MouseDown += new MouseButtonEventHandler(PătrățelMouseDown);
+            this.KeyUp += new KeyEventHandler(PătrățelKeyUp);
+            this.PreviewKeyDown += new KeyEventHandler(PătrățelPreviewKeyDown);
+        }
+
+        void PătrățelPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Right:
+                    if (this.Right != null)
+                    {
+                        this.Right.Focus();
+                    }
+                    e.Handled = true;
+                    return;
+                case Key.Left:
+                    if (this.Left != null)
+                    {
+                        this.Left.Focus();
+                    }
+                    e.Handled = true;
+                    return;
+                case Key.Up:
+                    if (this.Up != null)
+                    {
+                        this.Up.Focus();
+                    }
+                    e.Handled = true;
+                    return;
+                case Key.Down:
+                    if (this.Down != null)
+                    {
+                        this.Down.Focus();
+                    }
+                    e.Handled = true;
+                    return;
+            }
+        }
+
+        internal Pătrățel Left { get; set; }
+        internal Pătrățel Right { get; set; }
+        internal Pătrățel Up { get; set; }
+        internal Pătrățel Down { get; set; }
+
+        void PătrățelKeyUp(object sender, KeyEventArgs e)
+        {
+            char litera;
+            switch (e.Key)
+            {
+                case Key.A:
+                    litera = 'A';
+                    break;
+                case Key.B:
+                    litera = 'B';
+                    break;
+                case Key.C:
+                    litera = 'C';
+                    break;
+                case Key.D:
+                    litera = 'D';
+                    break;
+                case Key.E:
+                    litera = 'E';
+                    break;
+                case Key.F:
+                    litera = 'F';
+                    break;
+                case Key.G:
+                    litera = 'G';
+                    break;
+                case Key.H:
+                    litera = 'H';
+                    break;
+                case Key.I:
+                    litera = 'I';
+                    break;
+                case Key.J:
+                    litera = 'J';
+                    break;
+                case Key.K:
+                    litera = 'K';
+                    break;
+                case Key.L:
+                    litera = 'L';
+                    break;
+                case Key.M:
+                    litera = 'M';
+                    break;
+                case Key.N:
+                    litera = 'N';
+                    break;
+                case Key.O:
+                    litera = 'O';
+                    break;
+                case Key.P:
+                    litera = 'P';
+                    break;
+                case Key.Q:
+                    litera = 'Q';
+                    break;
+                case Key.R:
+                    litera = 'R';
+                    break;
+                case Key.S:
+                    litera = 'S';
+                    break;
+                case Key.T:
+                    litera = 'T';
+                    break;
+                case Key.U:
+                    litera = 'U';
+                    break;
+                case Key.V:
+                    litera = 'V';
+                    break;
+                case Key.W:
+                    litera = 'W';
+                    break;
+                case Key.X:
+                    litera = 'X';
+                    break;
+                case Key.Y:
+                    litera = 'Y';
+                    break;
+                case Key.Z:
+                    litera = 'Z';
+                    break;
+                case Key.Space:
+                    litera = default(char);
+                    break;
+                default:
+                    // don't change anything if we don't know what it is
+                    return;
+            }
+            this.Literă = litera;
+        }
+
+        void PătrățelMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!this.IsFocused)
+            {
+                this.Focus();
+            }
+        }
+
+        void PătrățelLostFocus(object sender, RoutedEventArgs e)
+        {
+            this.border.Background = Brushes.White;
+        }
+
+        void PătrățelGotFocus(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("cucu");
+            Keyboard.Focus(this);
+            this.border.Background = Brushes.PeachPuff;
+        }
+
+
+
+        public char Literă
+        {
+            get
+            {
+                string text = this.textBox.Text;
+                return text.SingleOrDefault();
+            }
+            set
+            {
+                Trace.Assert("ABCDEFGHIJKLMNOPQRSTUVWXYZ".Contains(value) || value == default(char));
+                if (value == default(char))
+                {
+                    this.textBox.Text = string.Empty;
+                }
+                else
+                {
+                    this.textBox.Text = value.ToString();
+                }
+            }
+        }
+    }
+}
