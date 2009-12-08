0393ace609352195104f71f24b8647c4111bbd42
vgrigoriu <vgrigoriu@cd77b782-3126-0410-85d0-d72a11eec3b5>
2009-12-08 21:51:14 +0000

    git-svn-id: http://rebus.googlecode.com/svn@17 cd77b782-3126-0410-85d0-d72a11eec3b5

---
+++ new/Rebus/RebusLib/Word.cs
@@ -0,0 +1,23 @@
+﻿namespace RebusLib
+{
+    /// <summary>
+    /// A weighted word.
+    /// </summary>
+    public class Word
+    {
+        /// <summary>
+        /// non-interesting constructor
+        /// </summary>
+        /// <param name="word">the actual string</param>
+        /// <param name="weight">it's weight</param>
+        public Word(string word, int weight)
+        {
+            this.Value = word;
+            this.Weight = weight;
+        }
+
+        public string Value { get; set; }
+
+        public int Weight { get; set; }
+    }
+}
---
+++ new/Rebus/RebusLib/Wordlist.cs
@@ -0,0 +1,70 @@
+﻿namespace RebusLib
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Keeps a list of weighted words, can retrieve those matching a regex
+    /// </summary>
+    public class Wordlist
+    {
+        private Dictionary<string, Word> wordCollection = new Dictionary<string, Word>();
+
+        public ICollection<Word> Words
+        {
+            get
+            {
+                return this.wordCollection.Values;
+            }
+        }
+
+        public ICollection<Word> WordsMatching(Regex regex)
+        {
+            return (from Word word in this.wordCollection.Values
+                   where regex.IsMatch(word.Value)
+                   select word).ToList();
+        }
+
+        public void Add(IEnumerable<string> words, int weight)
+        {
+            foreach (string word in words)
+            {
+                this.Add(word, weight);
+            }
+        }
+
+        public void Add(string value, int weight)
+        {
+            Word word = this.GetWord(value);
+            if (word == null)
+            {
+                word = new Word(value, weight);
+                this.wordCollection.Add(value, word);
+            }
+            else
+            {
+                Console.WriteLine("Already added word {0}", value);
+                word.Weight = System.Math.Max(weight, word.Weight);
+            }
+        }
+
+        public Word GetWord(string value)
+        {
+            Word word = null;
+            try
+            {
+                word = this.wordCollection[value];
+            }
+            catch (KeyNotFoundException) 
+            { 
+                /*ignore*/ 
+            }
+
+            return word;
+        }
+    }
+}
